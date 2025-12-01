caa8b717d2e884d71b6f2de5e4915752b340f7d9
keeganih <bonkdroid@gmail.com>
2025-12-01 16:57:33 -0500

    initial

---
+++ vol-encrypt/encrypt_vol_connector.h
@@ -0,0 +1,21 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright by The HDF Group.                                               *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This file is part of HDF5.  The full HDF5 copyright notice, including     *
+ * terms governing use, modification, and redistribution, is contained in    *
+ * the COPYING file, which can be found at the root of the source code       *
+ * distribution tree, or in https://support.hdfgroup.org/ftp/HDF5/releases.  *
+ * If you do not have access to either file, you may request a copy from     *
+ * help@hdfgroup.org.                                                        *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef _encrypt_vol_connector_H
+#define _encrypt_vol_connector_H
+
+/* The value must be between 256 and 65535 (inclusive) */
+#define ENCRYPT_VOL_CONNECTOR_VALUE    ((H5VL_class_value_t)12202)
+#define ENCRYPT_VOL_CONNECTOR_NAME     "encrypt_vol_connector"
+
+#endif /* _encrypt_vol_connector_H */
+
