695278307ab117f868d7842d0d81990981e40ad9
keeganih <bonkdroid@gmail.com>
2026-02-04 16:18:45 -0500

    setting up benchmark

---
+++ test/out.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+INS_DIR=$(realpath ../dependencies)
+
+echo "Installation Directory: ${INS_DIR}"
+
+export HDF5_DIR=${INS_DIR}/hdf5-ins
+export GCRYPT_ROOT_DIR=${INS_DIR}/gcrypt-ins
+export GPG_ERROR_ROOT_DIR=${INS_DIR}/gpgerror-ins
+
+rm -r out
+mkdir out
+cd out
+
+cmake .. \
+    -DENC_WRAPPER_ENABLE_NETTLE=Off
+
